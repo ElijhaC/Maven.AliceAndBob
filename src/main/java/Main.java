653eb2b5a2b26719709a9d63af0ac01deb880002
elijhac <ej9314@hotmail.com>
2022-10-30 22:47:29 -0400

    Checks if user is Alice or Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        //Make a scanner to get user guesses
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+
+        if (name.equals("Alice") || name.equals("Bob")){
+            System.out.println("Hello " + name + " welcome!");
+        }
+        else {
+            System.out.println("Goodbye");
+        }
     }
 }
